693079cac76a4013c164264600881ae7bbeb420c
mrstephenneal <stephen.neal14@gmail.com>
2018-09-14 12:35:23 -0400

    Added frozen_bundle to __all__ declaration

--- pybundle/__init__.py
+++ pybundle/__init__.py
@@ -1,4 +1,4 @@
-from pybundle.bundle import bundle_dir, resource_path
+from pybundle.bundle import bundle_dir, resource_path, frozen_bundle
 
 
-__all__ = ['bundle_dir', 'resource_path']
+__all__ = ['bundle_dir', 'resource_path', 'frozen_bundle']
