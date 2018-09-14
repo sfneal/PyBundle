f459cf1fb1ed125b5039f28822ec23ccb48aa9fd
mrstephenneal <stephen.neal14@gmail.com>
2018-09-14 12:36:33 -0400

    Updated to version 0.1.2
    Patched bug where forzen_bundle was not importable

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pybundle',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     url='https://github.com/mrstephenneal/pybundle',
     license='MIT',
