23f0587348b95055bdf18ff3451551db0a4f07a7
mrstephenneal <stephen.neal14@gmail.com>
2018-09-14 12:28:53 -0400

    Updated to version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pybundle',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     url='https://github.com/mrstephenneal/pybundle',
     license='MIT',
