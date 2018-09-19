112f296dc871b3fc37e3da5979234ae74bb50a21
mrstephenneal <stephen.neal14@gmail.com>
2018-09-19 11:02:18 -0400

    Updated to version 0.1.4
    Added frozen_bundle function to __all__ declaration

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pybundle',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     url='https://github.com/mrstephenneal/pybundle',
     license='MIT',
