cde51ac750bd1d7aea4d14d3b5a574c3c704e8a0
mrstephenneal <stephen.neal14@gmail.com>
2018-09-19 11:28:59 -0400

    Updated to version 1.0.1

--- PyBundle/__init__.py
+++ PyBundle/__init__.py
@@ -1,4 +1,4 @@
-from PyBundle.PyBundle import bundle_dir, resource_path, frozen_bundle
+from PyBundle.PyBundle import bundle_dir, resource_path, frozen
 
 
-__all__ = ['bundle_dir', 'resource_path', 'frozen_bundle']
+__all__ = ['bundle_dir', 'resource_path', 'frozen']
